b3250902f26d11857a65e507a71c003b6d5a89dd
ixboy <ismaelixboy@gmail.com>
2021-11-18 00:38:24 +0100

    add button and function to control the form submit

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,5 +1,7 @@
-import { default as React, useState } from 'react';
+import { React, useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { addBook } from '../redux/books/books';
 
 const AddBook = () => {
   const dispatch = useDispatch();
@@ -16,11 +18,24 @@ const AddBook = () => {
     setAuthor(author);
   };
 
+  const submitBookToStore = (e) => {
+    e.preventDefault();
+    const newBook = {
+      id: uuidv4(),
+      title,
+      author,
+    };
+
+    // dispatch an action and pass it the newBook object (your action's payload)
+    dispatch(addBook(newBook));
+    setTitle('');
+    setAuthor('');
+  };
 
   return (
     <div className="mx-auto m-4">
       <h2>ADD NEW BOOK</h2>
-      <form>
+      <form onSubmit={submitBookToStore}>
         <input type="text" id="titleId" value={title} onChange={addTitle} placeholder="Book title" />
         <input type="text" id="authorId" value={author} onChange={addAuthor} placeholder="Book Author" />
         <select type="select">
@@ -30,10 +45,10 @@ const AddBook = () => {
           <option value="test">Novels</option>
           <option value="test">Comic</option>
         </select>
-        <input type="submit" value="Add Book" />
+        <button type="submit">Add Book</button>
       </form>
     </div>
-  )
+  );
 };
 
 export default AddBook;
