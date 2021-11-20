e2fc40def2dc510eff10f61e1c9bbe5f0d1cf51a
ixboy <ismaelixboy@gmail.com>
2021-11-20 06:44:19 +0100

    remove bootstrap classes

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -17,44 +17,46 @@ const BookList = () => {
   const chapter = Math.round(Math.random() * (20 - 0) + 0);
 
   return (
-    <div className="card card-style mx-auto my-2">
+    <div className="book-card">
       {bookStore.map((book) => (
-        <div key={book.id} className="d-flex flex-row align-items-center justify-content-between my-2">
-          <div className="d-flex flex-column px-3">
-            <h5 className="text-secondary">{book.category}</h5>
-            <h3>
+        <div key={book.id}>
+          <div className="content">
+            <h5 className="category">{book.category}</h5>
+            <h3 className="title">
               {book.title}
             </h3>
-            <h5 className="author text-primary fw-light">{book.author}</h5>
+            <h5>{book.author}</h5>
             <div>
-              <ul className="d-flex flex-row text-decoration-none navbar-nav">
-                <li className="text-primary fw-light me-2">Comments</li>
-                <li> | </li>
-                <li>
+              <div className="links">
+                <a href="/#">Comments</a>
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <span className="vl" />
+                <a href="/#">
                   <RemoveBook id={book.id} book={book} />
-                </li>
-                <li> | </li>
-                <li className="text-primary fw-light mx-2">Edit</li>
-              </ul>
+                </a>
+                <span className="vl" />
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                <a href="/#">Edit</a>
+              </div>
             </div>
           </div>
-          <div className="d-flex flex-row px-3">
-            <div className="m-2"><CircularProgress color="primary" variant="determinate" value={progress} /></div>
-            <div className="flex-column">
-              <h2>
+          <div className="completion">
+            <div><CircularProgress color="primary" variant="determinate" value={progress} /></div>
+            <div>
+              <p>
                 {progress}
                 %
-              </h2>
-              <h5 className="completed">Completed</h5>
+              </p>
+              <h5>Completed</h5>
             </div>
           </div>
-          <div className="px-3">
+          <div className="progress">
             <h5>CURRENT CHAPTER</h5>
             <p>
               Chapter
               {` ${chapter}`}
             </p>
-            <button type="button" className="btn btn-primary">Update Progress</button>
+            <button type="button">Update Progress</button>
           </div>
         </div>
       ))}
