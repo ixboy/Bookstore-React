fb888366273087156f13ac189720b77975ca204e
ixboy <ismaelixboy@gmail.com>
2021-11-20 06:47:20 +0100

    refactor bookform, change classes and elements

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -6,12 +6,6 @@ import { addBook } from '../redux/books/books';
 const AddBook = () => {
   const dispatch = useDispatch();
   const [booktitle, setTitle] = useState('');
-  const [bookauthor, setAuthor] = useState('');
-
-  const addAuthor = (e) => {
-    const bookauthor = e.target.value;
-    setAuthor(bookauthor);
-  };
 
   const addTitle = (e) => {
     const booktitle = e.target.value;
@@ -22,36 +16,17 @@ const AddBook = () => {
     e.preventDefault();
     const eventCategory = document.getElementById('category');
     const bookcategory = eventCategory.options[eventCategory.selectedIndex].text;
-    const newBook = {
-      title: booktitle, category: bookcategory, item_id: uuidv4(), author: bookauthor,
-    };
+    const newBook = { title: booktitle, category: bookcategory, item_id: uuidv4() };
     dispatch(addBook(newBook));
     setTitle('');
-    setAuthor('');
-  };
-
-  const myStyle = {
-    fontSize: '1rem',
-    fontFamily: ['Montserrat', 'sans-serif'],
-  };
-
-  const myStyle2 = {
-    width: '30%',
-    height: '2.813rem',
-    padding: '1rem',
-    borderRadius: '4px',
-    border: 'solid 1px white',
-    backgroundColor: '#fff',
   };
 
   return (
-    <div className="card-style mx-auto m-4">
-      <h2 style={myStyle} className="fw-bold text-secondary">ADD NEW BOOK</h2>
-
+    <div className="form-div">
+      <span className="Text-Style-10">ADD NEW BOOK</span>
       <form onSubmit={submitBookToStore}>
-        <input type="text" id="titleId" value={booktitle} onChange={addTitle} style={myStyle2} placeholder="Book title" />
-        <input type="text" id="authorId" value={bookauthor} onChange={addAuthor} style={myStyle2} placeholder="Book Author" />
-        <select type="select" className="p-0 w-25" id="category" style={myStyle2}>
+        <input type="text" id="titleId" value={booktitle} onChange={addTitle} placeholder="Book title" />
+        <select type="select" id="category">
           <option value="Sci-Fi">Sci-fi</option>
           <option value="Learning">Learning</option>
           <option value="Programming">Programming</option>
